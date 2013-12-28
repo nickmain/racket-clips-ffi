555fb36677dcf4004bf0f8a14bf9facd348e1be7
Nick Main <david.nick.main@gmail.com>
2013-12-28 11:11:46 -0800

    multifield marshaling

--- src/clips-ffi.rkt
+++ src/clips-ffi.rkt
@@ -20,8 +20,7 @@
   [value _pointer]
   [begin _long]
   [end   _long]
-  [next  _pointer]
-))
+  [next  _pointer]))
 
 (native-lib libclips  
   _pointer CreateEnvironment  ()
@@ -52,50 +51,82 @@
   (make-dataObject #f 0 #f 0 0 #f))
 
 (define-cstruct _floatHashNode (
-  [next  _pointer]
+  [next  _floatHashNode-pointer]
   [count _long]
   [depth _int]
   [flags _uint32]
-  [contents _double]
-))
+  [contents _double]))
 
 (define-cstruct _integerHashNode (
-  [next  _pointer]
+  [next  _integerHashNode-pointer]
   [count _long]
   [depth _int]
   [flags _uint32]
-  [contents _llong]
-))
+  [contents _llong]))
 
 (define-cstruct _symbolHashNode (
-  [next  _pointer]
+  [next  _symbolHashNode-pointer]
   [count _long]
   [depth _int]
   [flags _uint32]
-  [contents _string]
-))
+  [contents _string]))
 
 (define-cstruct _externalAddressHashNode (
-  [next  _pointer]
+  [next  _externalAddressHashNode-pointer]
   [count _long]
   [depth _int]
   [flags _uint32]
   [externalAddress _pointer]
+  [type  _ushort]))
+
+(define-cstruct _field (
   [type  _ushort]
-))
+  [value _pointer]))
+
+(define-cstruct _multifield (
+  [busyCount _uint]
+  [depth     _short]
+  [length    _long]
+  [next      _multifield-pointer]
+  [fields    (_array _field 1)]))
 
 ; marshal a dataObject to a scheme value
 (define (dataObject->value dobj)
-  (case (dataObject-type dobj)
-    [(0) (floatHashNode-contents   (ptr-ref (dataObject-value dobj) _floatHashNode))]
-    [(1) (integerHashNode-contents (ptr-ref (dataObject-value dobj) _integerHashNode))]
-    [(2) (string->symbol (symbolHashNode-contents (ptr-ref (dataObject-value dobj) _symbolHashNode)))]
-    [(3 8) (symbolHashNode-contents  (ptr-ref (dataObject-value dobj) _symbolHashNode))]
-    [(4) #f ] ;;;;;;;;;;;;TODO
-    [(5) (externalAddressHashNode-externalAddress (ptr-ref (dataObject-value dobj) _externalAddressHashNode))]
-    [(6 7) (dataObject-value dobj)]
+  (clips-value->scheme-value (dataObject-value dobj) 
+                             (dataObject-type dobj)
+                             (dataObject-begin dobj)
+                             (dataObject-end dobj)))
+
+; multifield to vector
+(define (multifield->vector mf-ptr begin end)
+  (let* ([len (+ 1 (- end begin))]        
+         [mf  (ptr-ref mf-ptr _multifield)]
+         [fields (ptr-ref (array-ptr (multifield-fields mf))
+                          (_array _field (multifield-length mf)))]
+         [v (make-vector len)])
+    (let loop ([vidx 0])
+      (when (< vidx len)
+        (let* ([fld  (array-ref fields (+ vidx begin))]
+               [type (field-type fld)]
+               [val  (field-value fld)])
+          (vector-set! v vidx 
+                       (clips-value->scheme-value val type))
+          (loop (+ vidx 1)))))
+    v))
+  
+; marshal a CLIPS value to a scheme value
+(define (clips-value->scheme-value value-ptr type [begin 0] [end 0])
+  (case type
+    [(0)   (floatHashNode-contents   (ptr-ref value-ptr _floatHashNode))]
+    [(1)   (integerHashNode-contents (ptr-ref value-ptr _integerHashNode))]
+    [(2)   (string->symbol (symbolHashNode-contents (ptr-ref value-ptr _symbolHashNode)))]
+    [(3 8) (symbolHashNode-contents  (ptr-ref value-ptr _symbolHashNode))]
+    [(4)   (multifield->vector value-ptr begin end)]
+    [(5)   (externalAddressHashNode-externalAddress (ptr-ref value-ptr _externalAddressHashNode))]
+    [(6 7) value-ptr]
     [else #f]))
+
 
 ;;;TEST-->
 (define dobj (empty-dataObject))
-(EnvEval env "(create$ (sym-cat \"foo \" (+ 2 3 4.5)) a b c)" dobj)
+(EnvEval env "(create$ (sym-cat \"foo \" (+ 2 3 4.5)) a b c (str-cat a 12))" dobj)
